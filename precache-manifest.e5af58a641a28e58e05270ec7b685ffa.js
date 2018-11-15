772cd910bfa097676b8ed92d5bb5c8f703a85bf4
roguepnz <roguepnz@gmail.com>
2018-11-15 15:17:29 +0000

    vkapp test

---
+++ precache-manifest.e5af58a641a28e58e05270ec7b685ffa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "5f3d712e18ae86372967",
+    "url": "/static/js/main.5f3d712e.chunk.js"
+  },
+  {
+    "revision": "2dd891c45704ef41f3d5",
+    "url": "/static/js/1.2dd891c4.chunk.js"
+  },
+  {
+    "revision": "5f3d712e18ae86372967",
+    "url": "/static/css/main.5d38e389.chunk.css"
+  },
+  {
+    "revision": "6bc80414c3bbf8bd5e66ab142d94640c",
+    "url": "/index.html"
+  }
+];
